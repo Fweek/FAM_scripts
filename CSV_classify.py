48f178de66aa1a256be5297c103ba5a45383a4f8
Fweek <go.myco@gmail.com>
2018-02-05 16:14:11 -0800

    Update CSV_classify.py

--- CSV_classify.py
+++ CSV_classify.py
@@ -1,5 +1,4 @@
 #THIS IS A DUMMY FILE AND NEEDS TO BE REPLACED WITH THE REAL CLASSIFY SCRIPT
-
 import sys, os.path, shutil
 
 os.chdir(sys.argv[1])
@@ -7,8 +6,8 @@ os.chdir(sys.argv[1])
 if not os.path.exists('Output-Classified'):
 	os.makedirs('Output-Classified', )
 
-src_files = os.listdir(str(sys.argv[1]+'/Output-Combined')
+src_files = os.listdir(str(sys.argv[1])+'/Output-Combined')
 for file_name in src_files:
-    full_file_name = os.path.join(str(sys.argv[1]+'/Output-Combined', file_name)
+    full_file_name = os.path.join(str(sys.argv[1])+'/Output-Combined', file_name)
     if (os.path.isfile(full_file_name)):
-        shutil.copy(full_file_name, str(sys.argv[1]+'/Output-Classified')
+        shutil.copy(full_file_name, str(sys.argv[1])+'/Output-Classified')
