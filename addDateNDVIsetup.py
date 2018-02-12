bf3039cd4b1740012b124d4db5e6463cc7052d50
Fweek <go.myco@gmail.com>
2018-02-12 14:53:05 -0800

    Update and rename setup.py to addDateNDVIsetup.py

--- addDateNDVIsetup.py
+++ addDateNDVIsetup.py
@@ -1,7 +1,9 @@
 #Use by calling:
-# python setup.py build_ext --inplace
+# python addDateNDVIsetup.py build_ext --inplace
+#
 #If you get "ImportError: No module named Cython.Distutils" then check if you have cython installed.
 # pip install cython
+#
 #For more help: https://github.com/cython/cython/wiki/InstallingOnWindows
 #https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
 
