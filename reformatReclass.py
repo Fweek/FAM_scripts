c3f8052c3eded6f1924a566219762e9720aefc69
Fweek <go.myco@gmail.com>
2018-05-04 15:34:16 -0700

    Update reformatReclass.py

--- reformatReclass.py
+++ reformatReclass.py
@@ -1,150 +1,64 @@
-# Formatting and merging the CSV reclassified output from FAM algorithm with Geo-spatial farm field boundaries (DBF file)
-#  for the purpose of creating pivot and summary tables, figures, and maps, etc.
-#
-# Created by Carolyn Rosevelt, NASA-Ames/CSUMB Cooperative 02/14/18
-
-# Import libraries and read-in FAM output csv file.
 import pandas as pd
-import os, csv, sys
+import os, sys
 from simpledbf import Dbf5
 
-usage = "Formates the CDL codes in the reclassified CSV file in preparation to make pivot tables\n" + \
-        "usage: python reformatreclass.py <Directory path of reclassified file> <filename of DBF>"
+usage = "Further consolidates CDL codes into either cropped (2) or not cropped (10) in preparation to make pivot tables\n" + \
+        "usage: python reformatReclass.py <Directory path of reclassified file> <filename of DBF>"
 
 if len(sys.argv) < 2:  #number of arguments required
     print usage
     sys.exit(1)
 
 # Set working directory to user input (directory path of input files)
-os.chdir(sys.argv[1])
+os.chdir('C:\Users\Michael\Desktop\WA350\Reclassified')
 
+# Load in the DBF file and turn it into a CSV
+dbf = Dbf5('Joined_FieldPolys_FS_FAM_WA2018.dbf')
+dbf.to_csv('dbf.csv')
+
+# Add Seasonal and Annual columns and fill with reduced CDL codes
 # Loop through every file in the current working directory.
 for csvFilename in os.listdir('.'):
-    if not csvFilename.startswith('reclassified'): #skip the file if it does not start with reclassified in filename
+    if not csvFilename.startswith('reclassified'): #skip the file if it does not start with "reclassified" in filename
         print 'SKIPPING ' + csvFilename + ': Does not start with reclassified'
         continue
-        #sys.exit(1)  #For every file in the current working directory if it is not a CSV file then skip
 
     print('REFORMATTING ' + csvFilename + '...')
 
-#Change code values for first date
-inputFileName = csvFilename
-outputFileName = 'temp1.csv'
+    # Read in CSV
+    df = pd.read_csv(csvFilename)
 
-with open(inputFileName, 'rb') as inFile, open(outputFileName, 'wb') as outfile:
-    r = csv.reader(inFile)
-    w = csv.writer(outfile)
+    # Change columns names
+    df.rename(columns={'-9999': 'SIMS_ID'}, inplace=True)
+    df.rename(columns={df.columns[16]: 'Winter'}, inplace=True)
+    df.rename(columns={df.columns[17]: 'Summer'}, inplace=True)
+    df.rename(columns={df.columns[18]: 'Annual'}, inplace=True)
+    print df
+    
+    # Delete unnecessary columns
+    df = df.drop(df.columns[19:26], axis=1)
+    print df
+    
+    # If then statements for how to fill Winter, Summer, and Annual columns
+    # [ADD RULES HERE]
+    df['Winter'] = df.iloc[:, 13:16].apply(lambda x: 2 if 2 in x.values else x[0], axis=1)
+    df['Summer'] = df.iloc[:, 9:13].apply(lambda x: 2 if 2 in x.values else 10, axis=1)
 
-    #Change inputCSV to list
-    lines = list(r)
+    cols = ['Winter', 'Summer']
+    df['Annual'] = df[cols].apply(lambda x: 2 if 2 in x.values else 10, axis=1)
 
-    #Change header to add SIMS_ID label
-    lines[0][0] = 'SIMS_ID'
+    print(df)
+    
+    # Export dataframe to CSV
+    df.to_csv('reformattedReclass.csv')
 
-    for line in lines:
-        if line[1] == '4' or line[1] == '15':
-            w.writerow((line[0], '10', line[2], line[3], line[4], '10'))
-
-        elif line[1] == '8':
-            w.writerow((line[0], '2', line[2], line[3], line[4], '10'))
-
-        elif line[1] == '0':
-            w.writerow((line[0], 'NAN', line[2], line[3], line[4], '10'))
-
-        else:
-            w.writerow((line[0], line[1], line[2], line[3], line[4], "10"))
-
-
-
-#Change code values for second date
-inputFileName = 'temp1.csv'
-outputFileName = 'temp2.csv'
-
-with open(inputFileName, 'rb') as inFile, open(outputFileName, 'wb') as outfile:
-    r = csv.reader(inFile)
-    w = csv.writer(outfile)
-
-    #Change inputCSV to list
-    lines = list(r)
-
-    FirstDate = str(lines[0][1])
-
-    #Change header to add SIMS_ID label
-    lines[0][5] = 'YTD'+FirstDate
-
-    for line in lines:
-        if line[2] == '4' or line[2] == '16':
-            w.writerow((line[0], line[1], '10', line[3], line[4], line[5]))
-
-        elif line[2] == '8':
-            w.writerow((line[0], line[1], '2', line[3], line[4], line[5]))
-
-        elif line[2] == '0':
-            w.writerow((line[0], line[1], 'NAN', line[3], line[4], line[5]))
-
-        else:
-            w.writerow((line[0], line[1], line[2], line[3], line[4], line[5]))
-
-os.remove('temp1.csv')
-
-
-
-inputFileName = 'temp2.csv'
-outputFileName = 'temp3.csv'
-
-with open(inputFileName, 'rb') as inFile, open(outputFileName, 'wb') as outfile:
-    r = csv.reader(inFile)
-    w = csv.writer(outfile)
-
-    #Change inputCSV to list
-    lines = list(r)
-
-    for line in lines:
-        if line[1] == 'NAN':
-            w.writerow((line[0], line[1], line[2], line[3], line[4], line[5]))
-
-        elif int(line[1]) < int(10):
-            w.writerow((line[0], line[1], line[2], line[3], line[4], '2'))
-
-        else:
-            w.writerow((line[0], line[1], line[2], line[3], line[4], line[5]))
-
-os.remove('temp2.csv')
-
-
-
-inputFileName = 'temp3.csv'
-outputFileName = 'Reformatted_reclass.csv'
-
-with open(inputFileName, 'rb') as inFile, open(outputFileName, 'wb') as outfile:
-    r = csv.reader(inFile)
-    w = csv.writer(outfile)
-
-    #Change inputCSV to list
-    lines = list(r)
-
-    for line in lines:
-        if line[2] == 'NAN':
-            w.writerow((line[0], line[1], line[2], line[3], line[4], line[5]))
-
-        elif int(line[2]) < int(10):
-            w.writerow((line[0], line[1], line[2], line[3], line[4], '2'))
-
-        else:
-            w.writerow((line[0], line[1], line[2], line[3], line[4], line[5]))
-
-os.remove('temp3.csv')
-
-
-
-dbf = Dbf5(sys.argv[2])
-dbf.to_csv('temp4.csv')
-
-# Merge newly updated csv with dbf (csv) of the YEAR basemap
-a = pd.read_csv('Reformatted_reclass.csv')
-b = pd.read_csv('temp4.csv')
+# Merge newly updated csv with dbf (csv) using SIMS_ID as the common variable
+a = pd.read_csv('reformattedReclass.csv')
+b = pd.read_csv('dbf.csv')
 
 merged = a.merge(b, on='SIMS_ID')
 merged.to_csv('Appended_reclass.csv', index=False)
 
-os.remove('temp4.csv')
+# Remove intermediate files
+os.remove('dbf.csv')
+os.remove('reformattedReclass.csv')
