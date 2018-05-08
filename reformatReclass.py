17621e901c2bd960435913b9c88e3c2257e443c6
Fweek <go.myco@gmail.com>
2018-05-08 10:17:34 -0700

    Update reformatReclass.py

--- reformatReclass.py
+++ reformatReclass.py
@@ -10,10 +10,10 @@ if len(sys.argv) < 2:  #number of arguments required
     sys.exit(1)
 
 # Set working directory to user input (directory path of input files)
-os.chdir('C:\Users\Michael\Desktop\WA350\Reclassified')
+os.chdir(sys.argv[1])
 
 # Load in the DBF file and turn it into a CSV
-dbf = Dbf5('Joined_FieldPolys_FS_FAM_WA2018.dbf')
+dbf = Dbf5(sys.argv[2])
 dbf.to_csv('dbf.csv')
 
 # Add Seasonal and Annual columns and fill with reduced CDL codes
@@ -34,11 +34,11 @@ for csvFilename in os.listdir('.'):
     df.rename(columns={df.columns[17]: 'Summer'}, inplace=True)
     df.rename(columns={df.columns[18]: 'Annual'}, inplace=True)
     print df
-    
+
     # Delete unnecessary columns
     df = df.drop(df.columns[19:26], axis=1)
     print df
-    
+
     # If then statements for how to fill Winter, Summer, and Annual columns
     # [ADD RULES HERE]
     df['Winter'] = df.iloc[:, 13:16].apply(lambda x: 2 if 2 in x.values else x[0], axis=1)
@@ -48,7 +48,7 @@ for csvFilename in os.listdir('.'):
     df['Annual'] = df[cols].apply(lambda x: 2 if 2 in x.values else 10, axis=1)
 
     print(df)
-    
+
     # Export dataframe to CSV
     df.to_csv('reformattedReclass.csv')
 
